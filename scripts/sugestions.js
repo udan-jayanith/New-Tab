e45da944cb6176f5b0b982d29dc09bef2af4b319
Udan-Jayanith <j.udanjayanith@gmail.com>
2024-09-21 14:40:49 +0530

    nothing at all

--- scripts/sugestions.js
+++ scripts/sugestions.js
@@ -11,7 +11,7 @@ async function suggestions(value) {
 				`https://www.google.com/complete/search?client=chrome&q=${value}`
 			)
 	)
-	const dataTo = await response.json() 
+	const dataTo = await response.json()
 	const data = await JSON.parse(dataTo.contents)[1]
 
 	suggestionsList.innerHTML = ''
@@ -23,21 +23,36 @@ async function suggestions(value) {
 	})
 }
 
+let currentIndex = 0
+
 input.addEventListener('keyup', (event) => {
+	const suggestionItem = document.querySelectorAll('.suggestion-item')
+	suggestionItem[0].classList.add('suggestion-item-active')
+
+	removeClass()
+
+	if (event.key == 'ArrowDown') {
+		if (currentIndex < suggestionItem.length) currentIndex++
+		suggestionItem[currentIndex].classList.add('suggestion-item-active')
+	}
+
+	if (event.key == 'ArrowUp') {
+		if (currentIndex >= 0) currentIndex--
+		suggestionItem[currentIndex].classList.add('suggestion-item-active')
+	}
+
+	if (event.key == 'ArrowDown' || event.key == 'ArrowUp') return 0
 	suggestions(event.target.value)
 })
+
+function removeClass() {
+	document.querySelectorAll('.suggestion-item').forEach((el) => {
+		el.classList.remove('suggestion-item-active')
+	})
+}
 
 suggestionsList.addEventListener('click', (event) => {
 	if (event.target.className == 'suggestion-item') {
 		document.querySelector('.search-bar').value = event.srcElement.dataset.value
-	}
-})
-
-let currentIndex = 0
-
-addEventListener('keyup', (event) => {
-	// ArrowDown 
-	if (event.key == 'ArrowDown') {
-
 	}
 })
